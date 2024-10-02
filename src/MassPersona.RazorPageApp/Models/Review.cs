da3eefea7c2fe1a7f70b5f720676826cfc31e706
ponsongs <ponsongvilay@outlook.com>
2024-10-02 17:55:55 -0500

    Create Review model and added properties

---
+++ src/MassPersona.RazorPageApp/Models/Review.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MassPersona.RazorPageApp.Models
+{
+    [Table("Reviews")]
+    public class Review
+    {       
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("ReviewID")]
+        public int ReviewID { get; set; }
+        [Column("Title")]
+        public string Title { get; set; }
+        [Column("Category")]
+        public string Category { get; set; }
+        [Column("ReviewText")]
+        public string ReviewText { get; set; }
+        [Column("Rating")]
+        public int Rating { get; set; }
+        [Column("DateReviewed")]
+        public DateTime DateReviewed { get; set; }
+        
+    }
+}
