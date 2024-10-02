b5e00fd38d82db36266de3c6bbe087a06cbca126
ponsongs <ponsongvilay@outlook.com>
2024-10-02 17:57:53 -0500

    Create ApplicationDbContext

---
+++ src/MassPersona.RazorPageApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MassPersona.RazorPageApp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options) { }
+        public DbSet<Review> Reviews => Set<Review>();
+    }
+}
